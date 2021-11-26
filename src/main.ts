76883eac72312b5f790d78b7ddc07ebde840b03e
Quang Vo <thanhquang2608@gmail.com>
2021-11-26 12:21:18 +0700

    Implement check PR is WIP or not

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import * as core from '@actions/core'
-const payload = require(process.env.GITHUB_EVENT_PATH);
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const isWip = /\bwip\b/i.test(payload.pull_request.title);
-    core.setOutput("is_wip", isWip.toString());
+    const {eventName} = github.context
+    core.info(`Event name: ${eventName}`)
+
+    if (eventName !== 'pull_request') {
+      core.setFailed(
+        `Invalid event: ${eventName}, it should be use on pull_request`
+      )
+      return
+    }
+
+    const title = github.context.payload.pull_request?.title
+    const isWip = /\bwip\b/i.test(title)
+    core.setOutput('is_wip', isWip.toString())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
